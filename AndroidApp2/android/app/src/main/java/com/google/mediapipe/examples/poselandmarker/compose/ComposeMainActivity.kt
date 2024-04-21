6b7f10811976e384388ee62f62b0f878955235dc
Koren <aljosa.koren@razum.si>
2024-04-21 03:01:50 +0200

    Adding Compose on top

---
+++ AndroidApp2/android/app/src/main/java/com/google/mediapipe/examples/poselandmarker/compose/ComposeMainActivity.kt
@@ -0,0 +1,54 @@
+package com.google.mediapipe.examples.poselandmarker.compose
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.google.mediapipe.examples.poselandmarker.MainActivity
+
+class ComposeMainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            val context = LocalContext.current
+            val navController = rememberNavController()
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                NavHost(
+                    navController = navController,
+                    startDestination = "login"
+                ) {
+                    composable("login") {
+
+                    }
+                    composable("home") {
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun ButtonTest() {
+    val context = LocalContext.current
+    Button(onClick = {
+        //open the .MainActivity
+        val intent = Intent(context, MainActivity::class.java)
+        context.startActivity(intent)
+    }) {
+
+    }
+}
